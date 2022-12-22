54c6a8bc071a572ffeadb7881e28fdc96780a9b4
jithin <jithinpjose12@gmail.com>
2022-12-22 10:42:41 +0530

    #added test classes

---
+++ api-service-leave-forcasting/src/main/java/com/ibsplc/apiserviceleaveforcasting/form/TestForm.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ibsplc.apiserviceleaveforcasting.form;
+
+/**
+ *
+ * @author jithin123
+ */
+public class TestForm {
+    
+}
