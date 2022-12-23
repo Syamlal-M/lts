5ed75cf80dbfa8cc14399a56914833348af162a0
jithin <jithinpjose12@gmail.com>
2022-12-23 14:19:59 +0530

    #add db queries

---
+++ api-service-leave-forcasting/database/patch.sql
@@ -0,0 +1,18 @@
+--------------------23/12/2022-----------------
+CREATE TABLE leaveforecast.employee (
+    emp_id varchar(255) NOT NULL,
+    employee_name varchar(255) NOT NULL,
+    expedia_fg_name varchar(255) NOT NULL,
+    vendor_name varchar(255) NOT NULL,
+    job_title varchar(255) NOT NULL,
+    hm varchar(255) NOT NULL,
+    bill_rate varchar(255) NOT NULL,
+    country varchar(255) NOT NULL,
+    city varchar(255) NOT NULL,
+    sow varchar(255) NOT NULL,
+    org varchar(255) NOT NULL,
+    team varchar(255) NOT NULL,
+    billability varchar(255) NOT NULL,
+    remarks varchar(500),
+    PRIMARY KEY (emp_id)
+);
