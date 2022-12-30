aad3a2e08e0dd918bc9cbf99928a13bdfe20a36c
anmenon1 <121142793+anmenon1@users.noreply.github.com>
2022-12-30 11:40:23 +0530

    Update settings.js

--- api-service-leave-forcasting/front-end/src/components/settings.js
+++ api-service-leave-forcasting/front-end/src/components/settings.js
@@ -7,7 +7,7 @@ import { AgGridReact } from 'ag-grid-react';
 
 //dropdownlist = ("Self", "Team", "All");
 
-const datafetch = () => {
+
   const gridRef = useRef();
   const containerStyle = useMemo(() => ({ width: '100%', height: '100%' }), []);
   const gridStyle = useMemo(() => ({ height: '100%', width: '100%' }), []);
@@ -46,7 +46,7 @@ const setEditableRole = useCallback((role) => {
   
   gridRef.current.api.redrawRows();
 }, []);
-}
+
  const settings = () => {
   return(<div>
     <Box component="form" sx={{'& > :not(style)': { m: 1, width: '25ch' }, '& .MuiTextField-root': { m: 1, width: '25ch' },}} noValidate autoComplete="off">
