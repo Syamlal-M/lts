fdeed25dc2c6a453ca2d736b21cdad7bd6320a03
Angel Paulson <v-anpaulson@expediagroup.com>
2023-03-21 22:09:01 +0530

    fixed constants

---
+++ leave-forcasting/src/components/roles/constant.js
@@ -0,0 +1,16 @@
+const roles = [
+    {
+      value: '1',
+      label: 'admin',
+    },
+    {
+      value: '2',
+      label: 'self',
+    },
+    {
+      value: '3',
+      label: 'team',
+    }
+  ]
+
+  export {roles};
