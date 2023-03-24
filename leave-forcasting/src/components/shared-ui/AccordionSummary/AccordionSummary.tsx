d38f352a42c7e99af2fb8e9982fe2196b5b53238
Benin Tom Jose <A-10806@LE1669.local>
2023-03-24 13:11:21 +0530

    feat/ implement Accordion related components in shared-ui

---
+++ leave-forcasting/src/components/shared-ui/AccordionSummary/AccordionSummary.tsx
@@ -0,0 +1,16 @@
+import {
+    AccordionSummary as MuiAccordionSummary,
+    AccordionSummaryProps as MuiAccordionSummaryProps
+} from "@mui/material";
+
+type AccordionSummaryProps = MuiAccordionSummaryProps;
+
+const AccordionSummary = (props: AccordionSummaryProps) => {
+    const { ...rest } = props;
+
+    return (
+        <MuiAccordionSummary {...rest} />
+    );
+}
+
+export { AccordionSummary };
