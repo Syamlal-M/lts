89440fe116e52312184851fe7ffb2f2ccd309f12
Benin Tom Jose <A-10806@LE1669.local>
2023-03-24 13:15:33 +0530

    feat/ implement Alert and associated components in shared-ui

---
+++ leave-forcasting/src/components/shared-ui/Alert/Alert.tsx
@@ -0,0 +1,16 @@
+import {
+    Alert as MuiAlert,
+    AlertProps as MuiAlertProps
+} from "@mui/material";
+
+type AlertProps = MuiAlertProps;
+
+const Alert = (props: AlertProps) => {
+    const { ...rest } = props;
+
+    return (
+        <MuiAlert {...rest} />
+    );
+}
+
+export { Alert };
---
+++ leave-forcasting/src/components/shared-ui/AlertTitle/AlertTitle.tsx
@@ -0,0 +1,16 @@
+import {
+    AlertTitle as MuiAlertTitle,
+    AlertTitleProps as MuiAlertTitleProps
+} from "@mui/material";
+
+type AlertTitleProps = MuiAlertTitleProps;
+
+const AlertTitle = (props: AlertTitleProps) => {
+    const { ...rest } = props;
+
+    return (
+        <MuiAlertTitle {...rest} />
+    );
+}
+
+export { AlertTitle };
