a7a23c86af813dafc7a90d6f679bbcd64ce1b133
Abilashcv <abilashcveee@gmail.com>
2023-04-03 14:23:03 +0530

    chore: enabled CORS(#39)
    Co-authored-by: Abilash Varukutty <v-avarukutty@expediagroup.com>

--- api-service-leave-forcasting/src/main/java/com/ibsplc/apiserviceleaveforcasting/config/SwaggerConfig.java
+++ api-service-leave-forcasting/src/main/java/com/ibsplc/apiserviceleaveforcasting/config/SwaggerConfig.java
@@ -2,6 +2,9 @@ package com.ibsplc.apiserviceleaveforcasting.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -20,6 +23,16 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*");
+            }
+        };
     }
 
 }
