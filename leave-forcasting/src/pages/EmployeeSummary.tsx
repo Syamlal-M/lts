6d0438e2bbd654195ee0398f56f39bc3545b327b
Angel Paulson <v-anpaulson@expediagroup.com>
2023-04-17 13:32:39 +0530

    updated employee-service page

--- leave-forcasting/src/pages/EmployeeSummary.tsx
+++ leave-forcasting/src/pages/EmployeeSummary.tsx
@@ -1,26 +1,23 @@
 import { Box, DataGrid } from 'components/shared-ui';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import * as React from 'react';
 import EmployeeSummaryColumnList from 'data/EmployeeSummaryColumnList';
+import EmployeeSummaryService from 'service/EmployeeSummaryService';
 
-const EmplolyeeSummary = () => {
-	const [empList, setEmpList] = useState([]);
+const EmployeeSummary = () => {
+	const [empList, setEmpList] = React.useState<any>('');
+	const fetchEmployeeSummary = () => {
+		EmployeeSummaryService.fetchForecast()
+			.then((response) => {
+				setEmpList(response);
+			})
+			.catch((error) => console.log(error));
+	};
 
 	useEffect(() => {
-		var requestOptions = {
-			method: 'GET',
-			// headers: {
-			// 	role: cookies.get('role'),
-			// },
-		};
-
-		fetch('api/employee/search?limit=1500', requestOptions)
-			.then((response) => response.json())
-			.then((result) => {
-				console.log(result);
-				setEmpList(result.content);
-			})
-			.catch((error) => console.log('error', error));
+		fetchEmployeeSummary();
 	}, []);
+
 	return (
 		<Box sx={{ height: 400, maxWidth: 'calc(100vw - 36px)' }}>
 			<DataGrid
@@ -35,9 +32,10 @@ const EmplolyeeSummary = () => {
 						},
 					},
 				}}
+				pageSizeOptions={[5]}
 			/>
 		</Box>
 	);
 };
 
-export default EmplolyeeSummary;
+export default EmployeeSummary;
