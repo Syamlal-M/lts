662f61e1adc2792c6d6ab33912c7fd216381b486
Benin Tom Jose <A-10806@LE1669.local>
2023-04-18 18:17:44 +0530

    fix/ implement common themeOptions for darkMode and lightMode

---
+++ leave-forcasting/src/lib/mui/Theme.tsx
@@ -0,0 +1,41 @@
+import COLOR from 'styles/Color';
+import { PaletteMode } from '@mui/material';
+import { ThemeOptions, createTheme } from '@mui/material/styles';
+
+const getThemeOptions = (mode: PaletteMode): ThemeOptions => ({
+    typography: {
+        htmlFontSize: 10,
+        fontSize: 14
+    },
+    palette: {
+        mode: mode,
+        primary: {
+            main: COLOR.primary.main,
+            contrastText: COLOR.common.white
+        },
+        secondary: {
+            main: COLOR.secondary.main,
+            contrastText: COLOR.common.white
+        }
+    },
+    shape: {
+        borderRadius: 2,
+    },
+    components: {
+        MuiCardHeader: {
+            styleOverrides: {
+                title: {
+                    fontWeight: 600,
+                    textTransform: "capitalize"
+                }
+            }
+        }
+    },
+});
+
+
+const LightTheme = createTheme(getThemeOptions('light'));
+const DarkTheme = createTheme(getThemeOptions('dark'));
+
+
+export { LightTheme, DarkTheme };
