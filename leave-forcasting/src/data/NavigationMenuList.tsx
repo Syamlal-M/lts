db0e73b0d5f4cacb3bc8a4f4f2bb0c4f65e5558d
Benin Tom Jose <A-10806@LE1669.local>
2023-04-27 22:58:27 +0530

    fix/ implement permissions for left-hand-navigation-list

--- leave-forcasting/src/data/NavigationMenuList.tsx
+++ leave-forcasting/src/data/NavigationMenuList.tsx
@@ -5,25 +5,29 @@ const NavigationMenuList: NavigationList = [
 		"id": 1,
 		"urlPath": "/leave-forecast",
 		"label": "Home",
-		"icon": "home"
+		"icon": "home",
+		"permission": "LEAVE_FORCAST",
 	},
 	{
 		"id": 2,
 		"urlPath": "/employee-info",
 		"label": "Employee Info",
-		"icon": "date_range"
+		"icon": "date_range",
+		"permission": "EMPLOYEE_SUMMARY"
 	},
 	{
 		"id": 3,
 		"urlPath": "/reports",
 		"label": "Reports",
-		"icon": "assignment_icon"
+		"icon": "assignment_icon",
+		"permission": "LEAVE_REPORT"
 	},
 	{
 		"id": 4,
 		"urlPath": "/settings",
 		"label": "Roles",
-		"icon": "account_circle"
+		"icon": "account_circle",
+		"permission": "EMPLOYEE_MANAGEMENT"
 	}
 ];
 
