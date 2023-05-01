c770745bd60ebf040ae612051b024c9afc5ed3aa
Benin Tom Jose <A-10806@LE1669.local>
2023-05-01 18:37:58 +0530

    feat/ implement context to store select-list values for org, team, month, year, etc

--- leave-forcasting/src/App.tsx
+++ leave-forcasting/src/App.tsx
@@ -3,13 +3,16 @@ import { ThemeProvider } from "context/ThemeContext";
 import { CssBaseline } from "components/shared-ui";
 import PageRoutes from "pages/_Routes";
 import 'App.css';
+import { SelectListProvider } from "context/SelectListContext";
 
 function App() {
   return (
     <NavigationProvider>
       <ThemeProvider>
-        <CssBaseline />
-        <PageRoutes />
+        <SelectListProvider>
+          <CssBaseline />
+          <PageRoutes />
+        </SelectListProvider>
       </ThemeProvider>
     </NavigationProvider>
   );
