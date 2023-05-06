ac063606f48036c106d56eb7fadd65e3a2d464c7
Benin Tom Jose <A-10806@LE1669.local>
2023-05-06 17:42:35 +0530

    fix/ refactor code indentation in MonthList and Colors

--- leave-forcasting/src/data/MonthList.tsx
+++ leave-forcasting/src/data/MonthList.tsx
@@ -1,54 +1,54 @@
 import { KeyValueList } from "types/KeyValueList";
 
-const MonthList: KeyValueList = [
-    {
-      value: 'JANUARY',
-      label: 'JANUARY'
-    },
-    {
-      value: 'FEBRUARY',
-      label: 'FEBRUARY'
-    },
-    {
-      value: 'MARCH',
-      label: 'MARCH'
-    },
-    {
-      value: 'APRIL',
-      label: 'APRIL'
-    },
-    {
-      value: 'MAY',
-      label: 'MAY'
-    },
-    {
-      value: 'JUNE',
-      label: 'JUNE'
-    },
-    {
-      value: 'JULY',
-      label: 'JULY'
-    },
-    {
-      value: 'AUGUST',
-      label: 'AUGUST'
-    },
-    {
-      value: 'SEPTEMBER',
-      label: 'SEPTEMBER'
-    },
-    {
-      value: 'OCTOBER',
-      label: 'OCTOBER'
-    },
-    {
-      value: 'NOVEMBER',
-      label: 'NOVEMBER'
-    },
-    {
-      value: 'DECEMBER',
-      label: 'DECEMBER'
-    }
-];
+const MonthList = [
+  {
+    value: 'JANUARY',
+    label: 'JANUARY'
+  },
+  {
+    value: 'FEBRUARY',
+    label: 'FEBRUARY'
+  },
+  {
+    value: 'MARCH',
+    label: 'MARCH'
+  },
+  {
+    value: 'APRIL',
+    label: 'APRIL'
+  },
+  {
+    value: 'MAY',
+    label: 'MAY'
+  },
+  {
+    value: 'JUNE',
+    label: 'JUNE'
+  },
+  {
+    value: 'JULY',
+    label: 'JULY'
+  },
+  {
+    value: 'AUGUST',
+    label: 'AUGUST'
+  },
+  {
+    value: 'SEPTEMBER',
+    label: 'SEPTEMBER'
+  },
+  {
+    value: 'OCTOBER',
+    label: 'OCTOBER'
+  },
+  {
+    value: 'NOVEMBER',
+    label: 'NOVEMBER'
+  },
+  {
+    value: 'DECEMBER',
+    label: 'DECEMBER'
+  }
+] as const;
 
 export default MonthList;
